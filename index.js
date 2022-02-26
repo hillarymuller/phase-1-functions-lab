ea55424e820701bd603e83aec9d9a5fa7f8d8a7a
Hillary Muller <hillaryaward@gmail.com>
2022-02-26 15:48:47 -0500

    commit tree 9970164f8898aa76dedb14b4656dc1df184de43b

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42);
+}
+distanceFromHqInBlocks(50);
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+distanceFromHqInFeet();
+function distanceTravelledInFeet(start, destination) {
+return (Math.abs(destination - start) * 264);
+}
+distanceTravelledInFeet(34, 38);
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0;
+    }
+    else if (distanceTravelledInFeet(start, destination) < 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02;
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2000) {
+       return distanceTravelledInFeet(start, destination) > 2500 ? 'cannot travel that far' : 25;
+    }
+}
+calculatesFarePrice(34, 38);
